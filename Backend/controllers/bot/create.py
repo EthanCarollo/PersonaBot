501962e2aff9c6e4335e4ba17f8b156a97c9a1ef
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2024-11-29 22:43:08 +0100

    🎐 Start to implement create logics

--- Backend/controllers/bot/create.py
+++ Backend/controllers/bot/create.py
@@ -11,3 +11,30 @@ import os
 logger = logging.getLogger("persona_bot")
 create_bot_data_bp = Blueprint('create_bot_data_bp', __name__)
 supabase_client: SupabaseClient = SupabaseClient.get_instance()
+
+
+
+@create_bot_data_bp.route('/bot/create', methods=['POST'])
+def add_bot():
+    data = request.json
+
+    bot_public_id = data.get("bot_public_id")
+    if not bot_public_id:
+        return jsonify({"error": "Missing 'bot_public_id'"}), 400
+
+    bot_name = data.get("bot_name")
+    if not bot_name:
+        return jsonify({"error": "Missing 'bot_name'"}), 400
+    
+    # this field isnt necessary so i get it and if he is not here, i just don't use it
+    bot_description = data.get("bot_description")
+
+    token = data.get('token')
+    supabase_profile = supabase_client.get_profile_from_jwt(token)
+
+    if not supabase_profile:
+        return jsonify({"error": "Not a good token !"}), 400
+
+    supabase_client.create_bot(supabase_profile.data["id"], bot_public_id, bot_name)
+
+    return jsonify({"success": "Success !"})
